a435c1136ddf5e9d8a80d607fa81faf6dd80f8b5
aoba <ipbdipbd00@gmail.com>
2019-06-03 23:24:34 +0900

    userのリクエストスペックを修正

--- backend/spec/requests/users_apis_spec.rb
+++ backend/spec/requests/users_apis_spec.rb
@@ -1,38 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe "Users API", type: :request do
-  describe "ユーザーの作成" do
 
-    before do
-      @headers = { "Content-Type": "application/json" }
+  context "パラメータが有効な時" do
+    it "ユーザーが作成されること" do
+      allow(UserMailer).to receive_message_chain(:account_activation, :deliver_now)
+      user_params = {
+        name: 'example',
+        email: 'example@example.com',
+        password: 'password',
+        password_confirmation: 'password'
+      }
+      expect{
+        post users_path, params: user_params
+      }.to change{ User.count }.by(1)
+      expect(UserMailer).to have_received(:account_activation)
+      expect(response).to have_http_status "200"
     end
-
-    context "パラメータが有効な時" do
-      it "mailerが正しく機能すること" do
-        # user_params = ({
-        #   name: 'example',
-        #   email: 'example@example.com',
-        #   password: 'password',
-        #   password_confirmation: 'password'
-        # }).to_json
-        # post users_path, params: user_params, headers: @headers
-
-        post users_path, params: {
-          name: 'example',
-          email: 'example@example.com',
-          password: 'password',
-          password_confirmation: 'password'
-        }
-        # json = JSON.parse(response.body)
-        # expect(response).to have_http_status(:success)
-      end
-    end
-
-    context "無効なパラメータの時" do
-      it "statusが401であり、エラーの内容が返ってくること" do
-
-      end
-    end
-
   end
+
+  context "無効なパラメータの時" do
+    it "ユーザーが作成されないこと" do
+      user_params = {
+        name: '',
+        email: 'example@example.com',
+        password: 'password',
+        password_confirmation: 'password'
+      }
+      expect{
+        post users_path, params: user_params
+      }.to change{ User.count }.by(0)
+      expect(response).to have_http_status "401"
+    end
+  end
+
 end
