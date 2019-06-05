1b5a09e1b377bf3eb8a32960cf9bd3b682954653
aoba <ipbdipbd00@gmail.com>
2019-06-05 20:54:01 +0900

    Isolate repositories front/ & backend/

--- frontend/src/actions/randomConstants.ts
+++
@@ -1,7 +0,0 @@
-export const ADD_MEMBERS = 'RANDOM/ADD_MEMBERS';
-export const ADD_MEMBER = 'RANDOM/ADD_MEMBER';
-export const REMOVE_MEMBER = 'RANDOM/REMOVE_MEMBER';
-export const START = 'RANDOM/START';
-export const STOP = 'RANDOM/STOP';
-export const APOINT_USER = 'RANDOM/APOINT_USER';
-export const CLEAR_APOINT_USER = 'RANDOM/CLEAR_APOINT_USER';
--- frontend/src/sagas/index.ts
+++
@@ -1,6 +0,0 @@
-import { fork } from 'redux-saga/effects';
-import github from './github';
-
-export default function* rootSaga() {
-  yield fork(github);
-}
